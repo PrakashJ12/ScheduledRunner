7d59b2b6aa588bfe9383264f4371c253f0591702
Prakash Joseph <expect112@gmail.com>
2019-02-18 23:53:40 +0530

    Initial Version

--- ScheduledRunner/Service1.cs
+++ ScheduledRunner/Service1.cs
@@ -3,26 +3,62 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using System.Configuration;
 
 namespace ScheduledRunner
 {
     public partial class Service1 : ServiceBase
     {
+        Timer timer = new Timer(); // name space(using System.Timers;)  
         public Service1()
         {
             InitializeComponent();
         }
-
         protected override void OnStart(string[] args)
         {
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            int schedulerInterval = int.Parse(ConfigurationManager.AppSettings.Get("SchedulerIntervalMinutes")); //number in minutes  
+            timer.Interval = schedulerInterval * 60 * 1000; //number in milisecinds  
+            timer.Enabled = true;
         }
-
         protected override void OnStop()
         {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Service is recall at " + DateTime.Now);
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to.   
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
         }
     }
 }
